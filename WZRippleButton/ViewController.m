8760738880d084a1317bcf108f1870c4ffff754e
SatanWoo <applewuziqi@gmail.com>
2015-01-07 01:07:50 +0800

    Ziqi:Update Background and Effect

--- WZRippleButton/ViewController.m
+++ WZRippleButton/ViewController.m
@@ -21,6 +21,9 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background"]];
+    [self.view addSubview:imageView];
+    
     // Inner Flash Button with no text
     WZFlashButton *innerFlashButton = [[WZFlashButton alloc] initWithFrame:CGRectMake(100, 200, 200, 60)];
     innerFlashButton.backgroundColor = [UIColor colorWithRed:42.0f/255.0f green:62.0f/255.0f blue:80.0f alpha:1.0f];
@@ -36,7 +39,7 @@
     [self.view addSubview:innerFlashTextButton];
     
     // Outer Round Button
-    WZFlashButton *outerRoundFlashButton = [[WZFlashButton alloc] initWithFrame:CGRectMake(100, 50, 50, 50)];
+    WZFlashButton *outerRoundFlashButton = [[WZFlashButton alloc] initWithFrame:CGRectMake(50, 50, 50, 50)];
     outerRoundFlashButton.buttonType = WZFlashButtonTypeOuter;
     outerRoundFlashButton.layer.cornerRadius = 25;
     outerRoundFlashButton.flashColor = [UIColor colorWithRed:240/255.f green:159/255.f blue:10/255.f alpha:1];
@@ -44,9 +47,9 @@
     [self.view addSubview:outerRoundFlashButton];
     
     // Outer Rectangle Button
-    WZFlashButton *outerRectangleFlashButton = [[WZFlashButton alloc] initWithFrame:CGRectMake(180, 50, 100, 60)];
+    WZFlashButton *outerRectangleFlashButton = [[WZFlashButton alloc] initWithFrame:CGRectMake(200, 50, 100, 60)];
     outerRectangleFlashButton.buttonType = WZFlashButtonTypeOuter;
-    [outerRectangleFlashButton setText:@"Hello World!" withTextColor:[UIColor purpleColor]];
+    [outerRectangleFlashButton setText:@"Hello World!" withTextColor:[UIColor whiteColor]];
     outerRectangleFlashButton.flashColor = [UIColor colorWithRed:248.0f/255.f green:175.0f/255.f blue:160.0f/255.f alpha:1];
     outerRectangleFlashButton.backgroundColor = [UIColor colorWithRed:0 green:152.0f/255.0f blue:203.0f/255.0f alpha:1.0f];
     [self.view addSubview:outerRectangleFlashButton];
